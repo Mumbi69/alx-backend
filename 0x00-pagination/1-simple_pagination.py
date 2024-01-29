1cb1a81eacdbfaebeb56ebf363cc6cb5a2a1e637
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-30 01:20:15 +0300

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -57,9 +57,9 @@ class Server:
         - List[List]: A list containing rows of data for the specified page.
         """
         assert isinstance(page, int)\
-        and page > 0, "Page must be a positive integer."
+            and page > 0, "Page must be a positive integer."
         assert isinstance(page_size, int)\
-        and page_size > 0, "Page size must be a positive integer."
+            and page_size > 0, "Page size must be a positive integer."
 
         start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
