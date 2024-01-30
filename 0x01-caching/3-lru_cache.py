004716f98f8b44ae7984b2c4cc3f14c17b0ba3fc
Mumbi69 <winfriendmumbi@gmail.com>
2024-01-31 01:34:52 +0300

    LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,1 +1,30 @@
 #!/usr/bin/env python3
+""" LRUCache module"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache class that inherits from BaseCaching"""
+    def __init__(self):
+        """ Initialize LRUCache"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                # Discard the least recently used item (LRU algorithm)
+                lru_key = self.order.pop(0)
+                del self.cache_data[lru_key]
+                print("DISCARD: {}".format(lru_key))
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key"""
+        if key is not None and key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data[key]
+        return None
