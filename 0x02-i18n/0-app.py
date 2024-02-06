5452bb6dba817efa2211bfc4ab481bdb060962a4
Mumbi69 <winfriendmumbi@gmail.com>
2024-02-07 02:57:19 +0300

    Basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -3,11 +3,13 @@
 
 from flask import Flask, render_template
 
+
 app = Flask(__main__)
 
 
 @app.route('/')
 def Welcome():
+    """represents the function welcome"""
     return render_template('0-index.html')
 
 
