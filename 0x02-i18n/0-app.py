05c41fac3a08e99b4fc41adcd80480c89c5137b7
Mumbi69 <winfriendmumbi@gmail.com>
2024-02-07 02:54:54 +0300

    Basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """Basic flask app"""
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__main__)
 
 
 @app.route('/')
-
+def Welcome():
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
