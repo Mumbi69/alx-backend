3f85949fd847d92d6914fda8daee0e305aa401d4
Mumbi69 <winfriendmumbi@gmail.com>
2024-02-07 03:12:29 +0300

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+"""Basic flask app"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+babel = Babel(app)
+
+
+class Config:
+    """config class with available languages"""
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """renders template 1-index.html"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
